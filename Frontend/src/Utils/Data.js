7e4ca01bb1f65d99be48fd5777180445eefe77b1
Nishant Kathrotiya <113300377+nishantKathrotiya@users.noreply.github.com>
2025-02-12 10:48:48 +0530

    Added Faculty Names and IDS

--- Frontend/src/Utils/Data.js
+++ Frontend/src/Utils/Data.js
@@ -2,3 +2,18 @@ export const labNo = [104,105,107,108,109,110,114,116,118];
 export const timeSlot = ["09:10 - 11:10" , "11:10 - 12:10" , "12:10 - 02:10","02:20 - 04:20","04:20 - 06:20"];
 export const problemList = ["Everything Fine", "Computer Not Starting" ,"Operating System Crash" , "Power Supply Issue","Power Supply Issue" , "AC and Light  Issue"];
 export const purposeOfuse = ["Academic Work" , "Project Work","other"];
+export const facultyNames = {
+    375:"Amit Nayak",
+    473:"Hardik Jayswal",
+    8209:"Rajesh Patel",
+    6039:"Radhika Patel",
+    6060:"Akash Patel",
+    6045:"Sachin Patel",
+    6056:"Ritika Jani",
+    6076:"Shital Sharma",
+    6082:"Ashish Kataria",
+    6089:"Hitesh Makwana",
+    6094:"Pooja Chaudhry",
+    6095:"Dipika Damodar",
+    6066:"Chintak Raval"
+};
