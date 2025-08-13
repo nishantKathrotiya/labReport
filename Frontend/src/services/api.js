300423b97643642c9377ec5806791b32fd83c350
Nishant <mojonly813@gmail.com>
2025-08-13 22:31:09 +0530

    Endpoint Changes

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,8 +1,8 @@
-// const BASE_URL = "https://labreport.onrender.com";
-const BASE_URL = "http://localhost:4222";
+const BASE_URL = "https://labreport.onrender.com";
+// const BASE_URL = "http://localhost:4222";
 
 export const endpoints = {
   FORM: BASE_URL + "/api/v1/register",
   GET_DATA: BASE_URL + "/api/v1/dashboard",
-  DOWNLOAD : BASE_URL + "/api/v1/download",
+  DOWNLOAD: BASE_URL + "/api/v1/download",
 };
